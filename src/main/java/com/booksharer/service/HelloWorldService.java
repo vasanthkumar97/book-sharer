3f48466bcfb25b588c7581148eba45418471f2e6
Vasanth kumar <vasanthca1997@gmail.com>
2019-06-08 22:33:26 +0530

    Add service

---
+++ src/main/java/com/booksharer/service/HelloWorldService.java
@@ -0,0 +1,11 @@
+package com.booksharer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+
+    public String getHello(){
+        return "Hello World";
+    }
+}
